6887eff7706e1bab1b4a6896c8554bbd300e747b
Joanna Strzelecka <joastr1@gmail.com>
2018-01-15 11:04:06 +0100

    add js to the project

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+
+
+// DRUM
+
+ function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+  
+
+
+// ANCHOR
+
+$(document).ready(function() {
+    $('.anchor').click(function() {
+        var hash = $(this).attr('href')
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 50
+        }, 1000);
+        return false;
+    });
+});
+
+//POWIĘKSZANIE ZDJĘCIA
+
+$(document).ready(function() {
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}		
+	});
+});
